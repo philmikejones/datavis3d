a24db5e28183b2a98b9e8f005cc6c650bd120644
Phil Mike Jones <philmikejones@gmail.com>
2016-09-26 13:36:16 +0100

    Start house-prices prep script

--- data-raw/1-prep-house-prices.R
+++ data-raw/1-prep-house-prices.R
@@ -1,5 +1,11 @@
+
+# Packages ====
+library("readxl")
+
 download.file(paste0("http://www.ons.gov.uk/file?uri=/",
                      "peoplepopulationandcommunity/housing/datasets/",
                      "housepricestatisticsforsmallareas/current/",
                      "hpssa19952014_tcm77-407774.xls"),
               destfile = "inst/extdata/house_prices.xls")
+
+hp <- readxl::excel_sheets("inst/extdata/house_prices.xls")
