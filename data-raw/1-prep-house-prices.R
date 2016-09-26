27e26e39605baeebb88c5fa08acbbfda11ab0f02
Phil Mike Jones <philmikejones@gmail.com>
2016-09-26 13:01:11 +0100

    Code prep-house-prices.R

---
+++ data-raw/1-prep-house-prices.R
@@ -0,0 +1,5 @@
+download.file(paste0("http://www.ons.gov.uk/file?uri=/",
+                     "peoplepopulationandcommunity/housing/datasets/",
+                     "housepricestatisticsforsmallareas/current/",
+                     "hpssa19952014_tcm77-407774.xls"),
+              destfile = "inst/extdata/house_prices.xls")
